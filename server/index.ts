2333831f72cf27bc283c1cc1f61ac5c42b8f919d
@d.shurashov <d.shurashov@gmail.com>
2023-08-06 19:10:52 +0300

    .husky/pre-commit: npm run lint:fix

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,19 @@
 import express from "express"
 import { engine } from "express-handlebars"
 import path from "path"
- 
-const PORT = process.env.PORT || 3001
+
+const port = process.env.PORT ?? 3001
 const app = express()
 
 app.engine("handlebars", engine({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "handlebars")
 app.set("views", path.join(__dirname, "views"))
 
-
 app.get("/", (req, res) => {
-	res.render("home")
+    res.render("home")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}...`)
+app.listen(port, () => {
+    // eslint-disable-next-line
+	console.log(`Server is running on PORT ${port}...`)
 })
